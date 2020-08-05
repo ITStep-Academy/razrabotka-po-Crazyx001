6b80c9f63b645ef9552ceca0c2c391b60f1409e7
Даниэль Давлетшин <Superdanik2004@gmail.com>
2020-08-05 17:31:40 +0500

    HELP!

--- Lesson 6/Lesson 6.cpp
+++ Lesson 6/Lesson 6.cpp
@@ -120,19 +120,45 @@ int main9()
     return 0;
 }
 
-int main9()
+int main10()
 {
     int a = 1, b = 1, c = 1, d = 1;
 
     a++;
     
-    b = (25 + a) * b;
+    b *= 25 + a;
 
     c--;
 
     b -= 3 * d;
 
-    printf("%d %d", -123 / 10, -123 % 10);
+    printf("%d %d %d %d", a, b, c, d);
 
     return 0;
 }
+
+int main11()
+{
+    int x, d11, d12, d21, d22, d31, d32, d41, d42;
+
+    printf("Please input a number: ");
+    scanf_s("%d", &x);
+
+    d11 = x % 2;
+    d12 = x / 2;
+
+    d21 = d12 % 2;
+    d22 = d12 / 2;
+
+    d31 = d22 % 2;
+    d32 = d22 / 2;
+
+    d41 = d32 % 2;
+    d42 = d32 / 2;
+
+    printf("%d > %d%d%d%d", x, d41, d31, d21, d11);
+
+    return 0;
+}
+
+//12, 13, 14 и 15 я не смог решить без использования циклов, буду рад помощи!
