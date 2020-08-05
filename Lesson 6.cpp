d340603b6cb06326e1a429430bd1417bcbb3a510
Crazyx001 <47449484+Crazyx001@users.noreply.github.com>
2020-08-05 16:18:12 +0500

    It's my commit =)

---
+++ Lesson 6.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#define SECONDS_IN_HOUR 3600
+#define SECONDS_IN_MINUTE 60
+
+int main1()
+{
+    double number, fractPart;
+    int intPart;
+
+    printf("Please input a number: ");
+    scanf_s("%lf", &number);
+
+    intPart = (int)number;
+    fractPart = number - intPart;
+
+    printf("Input number = %.2f\n", number);
+    printf("Integral part = %d\n", intPart);
+    printf("Fractional part = %.2f\n", fractPart);
+
+    return 0;
+}
+
+int main2()
+{
+    int number;
+
+    printf("Please input a number: ");
+    scanf_s("%d", &number);
+
+    printf("%d", number % 10);
+
+    return 0;
+}
+
+int main()
+{
+    int seconds, hours, minutes;
+
+    printf("Please input a number: ");
+    scanf_s("%d", &seconds);
+
+    hours = seconds / SECONDS_IN_HOUR;
+    seconds -= hours * SECONDS_IN_HOUR;
+
+    minutes = seconds / SECONDS_IN_MINUTE;
+    seconds -= minutes * SECONDS_IN_MINUTE;
+
+    printf("%d hours, %d minutes, %d seconds", hours, minutes, seconds);
+
+    return 0;
+}
